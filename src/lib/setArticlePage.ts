38fa64b2dbe94e480007f3bbe4d9b9d5c2155933
eeenbnb <eee.kz.www@gmail.com>
2022-09-04 16:30:58 +0900

    add prisma.

--- src/lib/setArticlePage.ts
+++ src/lib/setArticlePage.ts
@@ -1,29 +1,26 @@
 import fs from "fs-extra";
 import ejs from "ejs";
 import prettier from "prettier";
-import { marked } from "marked";
 import MarkdownIt from "markdown-it";
+import hljs from "highlight.js";
 import type { Article } from "~/types/Article";
-
-var md = require("markdown-it")({
-  html: true,
-  linkify: true,
-  typographer: true,
-});
 
 const setArticlePage = (item: Article) => {
   const template = fs.readFileSync("src/template/article.ejs", { encoding: "utf-8" });
   const parsedItem = {
     ...item,
     body: new MarkdownIt({
+      highlight: function (code, lang) {
+        return hljs.highlightAuto(code, [lang]).value;
+      },
       html: true,
       linkify: true,
+      breaks: true,
       typographer: true,
     }).render(item.body),
   };
   const html = ejs.render(template, { data: parsedItem });
-  // prettier.format(html, { parser: "html" });
-  fs.writeFileSync(`docs/article/${item.title}.html`, html, { encoding: "utf-8" });
+  fs.writeFileSync(`docs/article/${item.title}.html`, prettier.format(html, { parser: "html" }), { encoding: "utf-8" });
 };
 
 export default setArticlePage;
