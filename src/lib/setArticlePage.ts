1ac74e831e16a563bdd5ceb51ab843d741bc9708
eeenbnb <eee.kz.www@gmail.com>
2022-09-04 15:41:10 +0900

    sample add.

--- src/lib/setArticlePage.ts
+++ src/lib/setArticlePage.ts
@@ -1,9 +1,29 @@
 import fs from "fs-extra";
+import ejs from "ejs";
+import prettier from "prettier";
+import { marked } from "marked";
+import MarkdownIt from "markdown-it";
 import type { Article } from "~/types/Article";
 
+var md = require("markdown-it")({
+  html: true,
+  linkify: true,
+  typographer: true,
+});
+
 const setArticlePage = (item: Article) => {
-  const body = item.body;
-  fs.writeFileSync(`docs/article/${item.title}.html`, body, { encoding: "utf-8" });
+  const template = fs.readFileSync("src/template/article.ejs", { encoding: "utf-8" });
+  const parsedItem = {
+    ...item,
+    body: new MarkdownIt({
+      html: true,
+      linkify: true,
+      typographer: true,
+    }).render(item.body),
+  };
+  const html = ejs.render(template, { data: parsedItem });
+  // prettier.format(html, { parser: "html" });
+  fs.writeFileSync(`docs/article/${item.title}.html`, html, { encoding: "utf-8" });
 };
 
 export default setArticlePage;
